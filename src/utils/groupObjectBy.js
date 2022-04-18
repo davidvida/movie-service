0c146790dc3246ffa4ae5f1f3d4ed9b5650b9f19
David Vida <david.vida@fundacion-jala.org>
2022-04-18 14:50:37 -0400

    groupObject in array

--- src/utils/groupObjectBy.js
+++ src/utils/groupObjectBy.js
@@ -1,11 +1,12 @@
 const groupObjectBy = (property, array) => {
-  return array.reduce((grouped, movie) => {
+  const groupedBy = array.reduce((grouped, movie) => {
     movie[property].forEach(propertyValue => {
       grouped[propertyValue.toLowerCase()] = grouped[propertyValue.toLowerCase()] || [];
       grouped[propertyValue.toLowerCase()].push(movie);
     });
     return grouped;
   }, Object.create(null));
+  return Object.keys(groupedBy).map(group => ({name: group, movies: groupedBy[group]}));
 };
 
 module.exports = groupObjectBy;
